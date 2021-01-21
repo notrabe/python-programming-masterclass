8ff2df44ddc58ae6511591add2d0d0d647aee985
Evan Barton <notrabe1011@gmail.com>
2021-01-20 23:01:50 -0500

    slicing

---
+++ stepping-into-the-world-of-python/HelloWorld/slice-back.py
@@ -0,0 +1,16 @@
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+backwards1 = letters[25:0:-1]
+print(backwards1)
+
+backwards2 = letters[::-1]
+print(backwards2)
+
+qpo = letters[16:13:-1]
+print(qpo)
+
+edcba = letters[4::-1]
+print(edcba)
+
+last8 = letters[26:17:-1]
+print(last8)
--- stepping-into-the-world-of-python/HelloWorld/strings2.py
+++ stepping-into-the-world-of-python/HelloWorld/strings2.py
@@ -1,20 +1,3 @@
 parrot = 'Norwegian Blue'
-print(parrot)
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
 
-print()
-print(parrot[-1])
-print(parrot[-14])
-
-print()
-print(parrot[3-14])
-print(parrot[4-14])
-print(parrot[9-14])
-print(parrot[3-14])
-print(parrot[6-14])
-print(parrot[8-14])
+print(parrot[0:6])
